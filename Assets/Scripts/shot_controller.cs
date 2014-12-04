06432b9e59142fac08229a5f5ddadfb37f9a11cd
hetpin <ongiavotu@gmail.com>
2014-12-04 14:17:43 +0900

    Draft version

---
+++ Assets/Scripts/shot_controller.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shot_controller : MonoBehaviour
+{
+
+		// Speed to be modified in the Inspector
+		public float speed = 60.0f;
+		
+		void Start ()
+		{
+				// Give the ball some initial movement direction
+				rigidbody2D.velocity = Vector2.one.normalized * speed;
+		}
+
+		void OnCollisionEnter2D (Collision2D coll)
+		{
+		if (coll.gameObject.tag == "boundary") {
+			//Hit boundary, bounce off
+			//Debug.Log ("wall collison");		
+		} else {
+			//Debug.Log ("other collison");	
+			//1. Zombie - thit luon
+			//2. Hero - Kill yourself
+		}
+
+		}
+}
---
+++ Assets/Scripts/zombie_move.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class zombie_move : MonoBehaviour
+{		
+		public Transform target;
+		/// Object speed
+		public float speed = 0.4f;
+
+		void Update ()
+		{
+				transform.position = Vector2.MoveTowards (transform.position, target.position, speed * Time.deltaTime);
+			Debug.Log (target.position);
+			Debug.Log (transform.position);
+		}
+		
+		void FixedUpdate ()
+		{
+				// Apply movement to the rigidbody
+//				rigidbody2D.velocity = movement;
+		}
+}
